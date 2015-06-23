c54bc447b4961bd17b9beb19d3fd2192bfcc5718
Christoph Lenggenhager <clenggenhager@gmail.com>
2015-06-23 19:15:23 +0200

    Minor: fix indentation

--- kcalcore/icaltimezones.cpp
+++ kcalcore/icaltimezones.cpp
@@ -1363,8 +1363,8 @@ QList<QDateTime> ICalTimeZoneSourcePrivate::parsePhase( icalcomponent *c,
         // the end date time specified must be in UTC
         // we can not guarantee correctness if this is not the case
         if (r.duration() == 0 && r.endDt().timeSpec() != KDateTime::Spec::UTC()) {
-            kDebug() << "UNTIL in RRULE must be specified in UTC";
-            return transitions;
+          kDebug() << "UNTIL in RRULE must be specified in UTC";
+          return transitions;
         }
 
         const DateTimeList dts = r.timesInInterval( kutcStart, maxTime );
